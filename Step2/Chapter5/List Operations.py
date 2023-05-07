a0229f3ba159ff742bbf938d956de3cb3f7d73e2
Shabnam <Shabnamaboulghasem@gmail.com>
2023-05-07 19:16:14 +0330

    removeing extra files

--- Step2/Chapter5/List Operations.py
+++
@@ -1,9 +0,0 @@
-print(len([[1,2]] * 3)) #3
-
-# Differences between in and index
-# 1- index tells where in a list a value can be found 
-# 2- in tells whether the value is in the list
-
-print(min(["a", "b", "c"]))
-# print(max([1,2, "three"])) raise TypeError exception: '>' not supported between instances of 'str' and 'int'
-print([1, 2, 3].count("one"))
--- Step2/Chapter5/Quick Check-len().py
+++
@@ -1,3 +0,0 @@
-print(len([0]))
-print(len([]))
-print(len([[1, 3, [4, 5],6], 7]))

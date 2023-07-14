778aa84c252afa1a6958b21cc1d84516538e84dc
Shabnam <Shabnamaboulghasem@gmail.com>
2023-07-14 20:36:52 +0330

    working with files

--- Step4/working with files.py
+++ Step4/working with files.py
@@ -1,12 +1,19 @@
-#import requests
-#import io
-#import pathlib
-#import pandas as pd
-#import warnings
-import xlrd,datetime,zipfile
+import requests
+# import io
+# import pathlib
+# import pandas as pd
+# import warnings
+import xlrd
+import datetime
+import zipfile
+import urllib.request
 
 
-#warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl')
+# url='https://www.histdata.com/download-free-forex-historical-data/?/excel/1-minute-bar-quotes/eurusd/2018'
+# urllib.request.urlretrieve(url,'TEST.txt')
+
+
+# warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl')
 
 # url='https://s17.picofile.com/d/8428234626/92fce4fb-232e-4624-87cf-338e4c444148/BarsaServiceClient.zip'
 # r = requests.get(url, stream=True)
@@ -22,11 +29,11 @@ workbook = xlrd. open_workbook('DAT_XLSX_EURUSD_M1_2018.xlsx')
 worksheet = workbook. sheet_by_index(0)
 row_count = worksheet.nrows
 col_count = worksheet.ncols
-for cur_row in range(0, 10):#row_count):
+for cur_row in range(0, 10):  # row_count):
     row = worksheet.row_values(cur_row, start_colx=0, end_colx=None)
-    row[0]=datetime.datetime(*xlrd.xldate_as_tuple(row[0], workbook.datemode)).strftime('%Y-%m-%d %H:%M:%S')
+    row[0] = datetime.datetime(
+        *xlrd.xldate_as_tuple(row[0], workbook.datemode)).strftime('%Y-%m-%d %H:%M:%S')
     print(row)
-
 
 
 # for cur_path in pathlib.Path('.').glob('*.xlsx'):
